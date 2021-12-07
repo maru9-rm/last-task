763c23dce77ca2727db6ddc0d683511986fb4f98
Ryota9 <ngm9dwf@gmail.com>
2021-12-08 01:08:17 +0900

    アバター画像を選択したらプロフィール画像を変更できるようにした。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,3 +20,21 @@ import axios from 'axios'
 import { csrfToken } from 'rails-ujs'
 
 axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
+
+
+
+
+window.addEventListener('load', () => {
+    const uploader = document.querySelector('.uploader');
+    uploader.addEventListener('change', (e) => {
+      const file = uploader.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        const image = reader.result;
+        document.querySelector('.avatar-image').setAttribute('src', image);
+      }
+      $('.submit-botton').removeClass('hidden')
+
+    });
+});

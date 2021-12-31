09dc2355616791508b618abea66f4669daa13e14
Ryota9 <ngm9dwf@gmail.com>
2021-12-31 23:16:41 +0900

    モデルのテスト。rspecの導入。

--- app/models/article.rb
+++ app/models/article.rb
@@ -14,11 +14,14 @@
 #
 class Article < ApplicationRecord
     belongs_to :user
+
+    validates :content, presence: true
+    validates :content, length: { maximum: 140 }
+
     has_many_attached :images
     has_many :likes, dependent: :destroy
     has_many :comments, dependent: :destroy
    
-    
     def display_created_at
         I18n.l(self.created_at, format: :default)
     end

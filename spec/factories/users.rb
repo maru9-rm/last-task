72a08777d1d1d8802059ee76e99f69c2e2fba1bc
Ryota9 <ngm9dwf@gmail.com>
2022-01-14 13:53:51 +0900

    system spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,11 @@ FactoryBot.define do
         user_name { Faker::Internet.user_name }
         email { Faker::Internet.email }
         password { 'password' }
+
+        trait :with_profile do
+            after :build do |user|
+                build(:profile, user: user)
+            end
+        end
     end
 end
